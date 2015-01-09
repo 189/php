f767d01ae4e45115daa6350b0687be14696391ed
189 <kv.wang87@gmail.com>
2015-01-09 15:39:09 +0800

    repository init

---
+++ php/test.php
@@ -0,0 +1,14 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Document</title>
+ </head>
+ <body>
+<?php
+	$str = '哈哈@huo@ff@xx';
+	echo explode('@', $str);
+?>
+
+ </body>
+ </html>
